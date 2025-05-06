1b4db5a94cb2e61630b275b2f75b4874989057ee
Giorgi Dolidze <gio.dolidze1234@gmail.com>
2025-05-06 22:47:45 +0400

    added extension class to API layer

---
+++ AuthService.API/Extensioms/ServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthService.API.Extensioms;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddPresentation(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+}
--- AuthService.API/Program.cs
+++ AuthService.API/Program.cs
@@ -1,12 +1,11 @@
+using AuthService.API.Extensioms;
 using AuthService.Infrastructure.Extensions;
 using AuthService.Infrastructure.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddInfrastructure(builder.Configuration);
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddPresentation();
 
 var app = builder.Build();
 
